f566c13d8d9e1e3ebf20078f3c986787e04c9b72
ajapadam <89146191+ajapadam@users.noreply.github.com>
2022-05-23 01:53:56 +0700

    Create directlink.js

---
+++ directlink.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://besidesaffluentcurrently.com/xsp5skzy?key=c379d569fe6a4f930da0afe372717c59', '_blank');
+	})
